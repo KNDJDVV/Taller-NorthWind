3935ff9b638a0293747d6c34f855d034ba963fc1
Juan Valencia <vjuandavid03@gmail.com>
2024-10-16 21:05:03 -0500

    NorthWind.ConsoleApp

---
+++ NorthWind.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using NorthWind.ConsoleApp.Services;
+using NorthWind.Entities.Interfaces;
+
+IUserActionWriter Writer = default;
+
+AppLogger Logger = new AppLogger(Writer);
+Logger.WriteLog("Application started");
+
+
+
