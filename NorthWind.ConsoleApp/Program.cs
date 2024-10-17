0011ca44b29e8e54482db370343f5bbf30643d84
Juan Valencia <vjuandavid03@gmail.com>
2024-10-17 18:47:57 -0500

    Debug

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -2,10 +2,14 @@
 using NorthWind.Entities.Interfaces;
 using NorthWind.Writers;
 
-IUserActionWriter Writer = new ConsoleWriter();
+IUserActionWriter Writer = new DebugWriter();
 
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Application started");
+
+ProductService Service = new ProductService(Writer);
+Service.Add("Demo", "Azucar refinada");
+
 
 
 
--- NorthWind.Entities/GlobalUsings.cs
+++ NorthWind.Entities/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using NorthWind.Entities.Interfaces;
 
 global using NorthWind.Entities.ValueObjects;
+
+global using System.Diagnostics;
